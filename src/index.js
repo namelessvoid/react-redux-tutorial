5863cc6ad78c7429a0ed7c5ba6a99119922bfb6f
Simon Kerler <s.kerler@fielmann.com>
2018-03-19 14:02:51 +0100

    Half way connecting react->redux (dispatch is working)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import { Provider, connect } from "react-redux";
 import store from "./store/index";
 import { setMark } from "./actions/index";
 
@@ -16,87 +17,93 @@ function Square(props) {
     );
 }
 
-  class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        };
-    }
-
-    renderSquare(i) {
-      return <Square value={ this.state.squares[i] } onClick={() => this.handleClick(i)}/>;
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-
-        if(squares[i] !== null  || this.calculateWinner(squares)) {
-            return;
-        }
-
-        squares[i] = this.state.xIsNext ? 'x' : 'o';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext
-        });
-    }
-
-    calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-
-        return null;
-    };
-
-    render() {
-        const winner = this.calculateWinner(this.state.squares);
-        let status;
-
-        if(winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
-        }
-
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+const mapStateToProps = state => {
+  return {
+    squares: state.squares,
+    xIsNext: state.xIsNext
   }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setMark: (mark, square) => dispatch(setMark(mark, square))
+  };
+};
+
+class ConnectedBoard extends React.Component {
+  renderSquare(i) {
+    return <Square value={ this.props.squares[i] } onClick={() => this.handleClick(i)}/>;
+  }
+
+  handleClick(i) {
+      if(this.props.squares[i] !== null  || this.calculateWinner(this.props.squares)) {
+          return;
+      }
+
+      this.props.setMark(this.props.xIsNext ? "x" : "o", i);
+      // squares[i] = this.props.xIsNext ? 'x' : 'o';
+      // this.setState({
+      //     squares: squares,
+      //     xIsNext: !this.state.xIsNext
+      // });
+  }
+
+  calculateWinner(squares) {
+      const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+      ];
+
+      for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+              return squares[a];
+          }
+      }
+
+      return null;
+  };
+
+  render() {
+      const winner = this.calculateWinner(this.props.squares);
+      let status;
+
+      if(winner) {
+          status = 'Winner: ' + winner;
+      } else {
+          status = 'Next player: ' + (this.props.xIsNext ? 'x' : 'o');
+      }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+const Board = connect(mapStateToProps, mapDispatchToProps)(ConnectedBoard);
 
   class Game extends React.Component {
     render() {
@@ -117,6 +124,8 @@ function Square(props) {
   // ========================================
 
   ReactDOM.render(
-    <Game />,
+    <Provider store={store}>
+      <Game />
+    </Provider>,
     document.getElementById('root')
   );
